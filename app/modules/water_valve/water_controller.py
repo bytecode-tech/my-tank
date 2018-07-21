8e293c3bfffe7c6760d19099a7ae415fe1410391
Josh Moore <josh.moore@bytecodetechnologies.com>
2018-07-21 13:39:28 -0500

    Fix method calls

--- app/modules/water_valve/water_controller.py
+++ app/modules/water_valve/water_controller.py
@@ -5,14 +5,14 @@ water_controller = Blueprint('water-controller', __name__, url_prefix='/api/wate
 
 @water_controller.route('/', methods=["GET"])
 def api_water_control():
-    return {'water-on-status': water.Relay.value()}
+    return {'water-on-status': water.value()}
 
 @water_controller.route('/on', methods=["POST"])
 def api_water_on():
     water.on()
-    return {'water-on-status': water.Relay.value()}
+    return {'water-on-status': water.value()}
 
 @water_controller.route('/off', methods=["POST"])
 def api_water_off():
     water.off()
-    return {'water-on-status': water.Relay.value()}
+    return {'water-on-status': water.value()}
