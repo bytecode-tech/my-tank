6934b66982f49187cb60f375a6129259f82665f3
Josh Moore <josh.moore@bytecodetechnologies.com>
2019-01-22 15:41:35 -0600

    refactor scheduler

--- app/modules/schedule/schedule_controller.py
+++ app/modules/schedule/schedule_controller.py
@@ -3,14 +3,6 @@ from .schedule import Scheduler
 
 schedule_controller = Blueprint('schedule-controller', __name__, url_prefix='/api/schedule')
 
-def job_response(job):
-    return {
-        'schedule': job.slices.render(),
-        'command': job.command,
-        'comment' : job.comment,
-        'enabled' : job.is_enabled()
-    }
-
 @schedule_controller.route('/', methods=["GET"])
 def api_schedule_control():
     scheduler = Scheduler()
