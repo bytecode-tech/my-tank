67473dbbccb394568ae18d1a8ae5a8178368fd58
Josh Moore <josh.moore@bytecodetechnologies.com>
2019-02-20 22:00:48 -0600

    wrap i2c soil open

--- app/modules/soil_sensor/soil.py
+++ app/modules/soil_sensor/soil.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python
 from board import SCL, SDA
 import busio
+import logging
 
 from adafruit_seesaw.seesaw import Seesaw
 
 MIN = 320
 MAX = 480
 
-i2c_bus = busio.I2C(SCL, SDA)
-ss = Seesaw(i2c_bus, addr=0x36)
+try:
+    i2c_bus = busio.I2C(SCL, SDA)
+    ss = Seesaw(i2c_bus, addr=0x36)
+except:
+    logging.exception('Could not open soil moisture sensor')
+
 
 def moist():
     raw_value = ss.moisture_read()
@@ -16,5 +21,6 @@ def moist():
         value = (((raw_value - MIN) / (MAX - MIN)) * 100)
         return int(round(value))
     else:
+        logging.exception('Moisture value out of range')
         raise Exception('Moisture value out of range')
     
