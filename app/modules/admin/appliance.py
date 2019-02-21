9acaa83b42c61d92a0565d9e5469aca4d24c5a74
Josh Moore <josh.moore@bytecodetechnologies.com>
2019-02-20 23:56:17 -0600

    add status for zero appliance

--- app/modules/admin/appliance.py
+++ app/modules/admin/appliance.py
@@ -20,4 +20,4 @@ def applianceState():
     sysbus = dbus.SystemBus()
     systemd1 = sysbus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
     manager = dbus.Interface(systemd1, 'org.freedesktop.systemd1.Manager')
-    return manager.ActiveState
+    return str(manager.ActiveState)
