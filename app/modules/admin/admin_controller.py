f2b9771db59c4c238560679b223c36a3f17e759f
Josh Moore <josh.moore@bytecodetechnologies.com>
2019-02-21 21:15:12 -0600

    add restart method for exporter

--- app/modules/admin/admin_controller.py
+++ app/modules/admin/admin_controller.py
@@ -20,13 +20,22 @@ def api_admin_server_status():
     return {'zero-appliance': applianceState, 'zero-exporter': exporterState}
 
 @admin_controller.route('/server/appliance', methods=["GET", "POST"])
-def api_admin_appliance_restart():
+def api_admin_appliance():
     if request.method == "POST":
         status = appliance.applianceRestart()
         return {'applianceRestartStatus': status}
     elif request.method == "GET":
-        status = appliance.applianceState()
-        return {'applianceStatus': status }
+        state = appliance.applianceState()
+        return {'applianceState': state }
+
+@admin_controller.route('/server/exporter', methods=["GET", "POST"])
+def api_admin_exporter():
+    if request.method == "POST":
+        status = appliance.exporterRestart()
+        return {'exporterRestartStatus': status}
+    elif request.method == "GET":
+        state = appliance.exporterState()
+        return {'exporterState': state }
 
 
 
