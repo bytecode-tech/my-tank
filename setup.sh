fc645ac1b0b3ea56dc6b9dbbc93409ad648b975c
Josh Moore <josh.moore@bytecodetechnologies.com>
2019-02-26 22:45:26 -0600

    add fix file

--- setup.sh
+++ setup.sh
@@ -26,6 +26,6 @@ sudo grep -q '^dtoverlay=w1-gpio' /boot/config.txt && sudo sed -i 's/^dtoverlay=
 
 
 # run prometheus docker image
-docker run -dit --restart unless-stopped --net=host -p 9090:9090 -v /home/pi/zero-appliance/prometheus.yml:/etc/prometheus/prometheus.yml -v prometheus-data:/var/lib/prometheus/ faucet/prometheus-pi
+docker run -dit --restart unless-stopped --net=host -p 9090:9090 --name weegrow_prometheus -v /home/pi/zero-appliance/prometheus.yml:/etc/prometheus/prometheus.yml -v prometheus-data:/var/lib/prometheus/ faucet/prometheus-pi
 
-sudo  docker run --net=host -d -p 80:80 --restart unless-stopped joshdmoore/aspen-app:latest
+docker run --net=host -d -p 80:80 --restart unless-stopped --name weegrow_app joshdmoore/aspen-app:latest
