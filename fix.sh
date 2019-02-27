1b653560d55d8826e0cd5cd22f6044976a1b1a85
Josh Moore <josh.moore@bytecodetechnologies.com>
2019-02-27 01:18:09 -0600

    change fix script

--- fix.sh
+++ fix.sh
@@ -9,7 +9,9 @@ sudo pip3 install uwsgi -I --no-cache-dir
 
 sudo docker pull joshdmoore/aspen-app
 
-sudo ./scripts/node_exporter_service.sh
+cd ./scripts
+sudo ./node_exporter_service.sh
+cd ..
 
 sudo docker run -dit --restart unless-stopped --net=host -p 9090:9090 --name weegrow_prometheus -v /home/pi/zero-appliance/prometheus.yml:/etc/prometheus/prometheus.yml -v prometheus-data:/var/lib/prometheus/ faucet/prometheus-pi
 sudo docker run --net=host -d -p 80:80 --restart unless-stopped --name weegrow_app joshdmoore/aspen-app:latest
