289d378aede01504313b87f303699fb62b427352
Josh Moore <josh.moore@bytecodetechnologies.com>
2019-02-26 22:53:02 -0600

    add pull for docker container

--- fix.sh
+++ fix.sh
@@ -3,5 +3,7 @@
 sudo systemctl stop zero-exporter
 sudo rm -rf /etc/systemd/system/zero-exporter.service 
 
-docker run -dit --restart unless-stopped --net=host -p 9090:9090 --name weegrow_prometheus -v /home/pi/zero-appliance/prometheus.yml:/etc/prometheus/prometheus.yml -v prometheus-data:/var/lib/prometheus/ faucet/prometheus-pi
-docker run --net=host -d -p 80:80 --restart unless-stopped --name weegrow_app joshdmoore/aspen-app:latest
+sudo docker pull joshdmoore/aspen-app
+
+sudo docker run -dit --restart unless-stopped --net=host -p 9090:9090 --name weegrow_prometheus -v /home/pi/zero-appliance/prometheus.yml:/etc/prometheus/prometheus.yml -v prometheus-data:/var/lib/prometheus/ faucet/prometheus-pi
+sudo docker run --net=host -d -p 80:80 --restart unless-stopped --name weegrow_app joshdmoore/aspen-app:latest
