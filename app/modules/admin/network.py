d28fb32c3e0bf1e72847b01d5ad58e8a359ba452
Josh Moore <josh.moore@bytecodetechnologies.com>
2019-06-13 20:42:59 -0500

    fix ssid scan

--- app/modules/admin/network.py
+++ app/modules/admin/network.py
@@ -3,21 +3,22 @@ from twisted.internet.selectreactor import SelectReactor
 import threading
 import time
 
-reactor = SelectReactor()
-threading.Thread(target=reactor.run, kwargs={'installSignalHandlers': 0}).start()
-time.sleep(0.1)  # let reactor start
-
-driver = WpaSupplicantDriver(reactor)
-
-supplicant = driver.connect()
-
-interface = supplicant.get_interface('wlan0')
-
 def scan():
+    reactor = SelectReactor()
+    threading.Thread(target=reactor.run, kwargs={'installSignalHandlers': 0}).start()
+    time.sleep(0.5)  # let reactor start
+
+    driver = WpaSupplicantDriver(reactor)
+
+    supplicant = driver.connect()
+
+    interface = supplicant.get_interface('wlan0')
+
     scan_results = interface.scan(block=True)
     ssids = []
-    
+
     for bss in scan_results:
         ssids.append(bss.get_ssid())
-    
+
+    reactor.stop()
     return ssids
