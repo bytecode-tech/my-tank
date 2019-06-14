a8c0d4c3091fbd1758f5eea179fc4f3a3accd565
Josh Moore <josh.moore@bytecodetechnologies.com>
2019-06-14 12:35:00 -0500

    fix serializable errors

--- app/modules/admin/network.py
+++ app/modules/admin/network.py
@@ -3,6 +3,31 @@ from twisted.internet.selectreactor import SelectReactor
 import threading
 import time
 
+class WifiNetwork():
+    def __init__(self, *args, **kwargs):
+        self.bssid = None
+        self.channel = None
+        self.frequency = None
+        self.mode = None
+        self.type = None
+        self.privacy = None
+        self.signal_dbm = None
+        self.signal_quality = None
+
+
+        if 'bss' in kwargs:
+            bss = kwargs.get('bss')
+
+            self.bssid = bss.get_bssid()
+            self.channel = bss.get_channel()
+            self.frequency = bss.get_frequency()
+            self.mode = bss.get_mode()
+            self.type = bss.get_network_type()
+            self.privacy = bss.get_privacy()
+            self.signal_dbm = bss.get_signal_dbm()
+            self.signal_quality = bss.get_signal_quality()
+            
+
 def scan():
     reactor = SelectReactor()
     threading.Thread(target=reactor.run, kwargs={'installSignalHandlers': 0}).start()
@@ -18,8 +43,7 @@ def scan():
     networks = []
 
     for bss in scan_results:
-        networks.append(bss)
+        networks.append(WifiNetwork(bss=bss))
 
     reactor.stop()
     return networks
-
