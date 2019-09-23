f7b3287b4e7151d82c0ad85017263a68415955fb
Josh Moore <josh.moore@bytecodetechnologies.com>
2019-09-23 14:22:17 -0500

    update device save and lookup

--- app/modules/devices/manager.py
+++ app/modules/devices/manager.py
@@ -23,21 +23,43 @@ def add_device(device: Device):
 def retrieve_device(encoded_alias):
     device = _DEVICES.get(encoded_alias)
 
-    if device is None:
-        file_path = _DATA_DIR + encoded_alias + '.json'
-        if path.exists(file_path):
-            f = open(file_path)
-            json_data = f.read()
-            if json_data:
-                device_props = json.loads(json_data)
-                device_class = device_props.get('class')
-                if device_class is 'OnboardReplay':
-                    device = OnboardReplay(device_props['alias'], device_props['gpio'])
-                else:
-                    device = TplinkPlug(device_props['alias'], device_props['host'])
+    if device:
+        f = open(_DATA_DIR + device.encoded_alias + '.json', 'w+')
+        device.save(f)
+    else:
+        if device is None:
+            file_path = _DATA_DIR + encoded_alias + '.json'
+            if path.exists(file_path):
+                device = _retrieve_device_from_file(file_path)
+
+            #if lookup worked, add to devices 
+            if device:
+                _DEVICES[device.encoded_alias] = device
 
     return device
 
 def retrieve_devices():
+    for file_path in os.listdir(_DATA_DIR):
+        device = None
+        if file_path.endswith(".json"):
+            device = _retrieve_device_from_file(file_path)
+
+        #if lookup worked, add to devices 
+        if device:
+            _DEVICES[device.encoded_alias] = device
+
     return _DEVICES.values()
         
+def _retrieve_device_from_file(file_path):
+    device = None
+    f = open(file_path)
+    json_data = f.read()
+    if json_data:
+        device_props = json.loads(json_data)
+        device_class = device_props.get('class')
+        if device_class is 'OnboardReplay':
+            device = OnboardReplay(device_props['alias'], device_props['gpio'])
+        else:
+            device = TplinkPlug(device_props['alias'], device_props['host'])
+
+    return device
