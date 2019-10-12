51d271f1d9c776f478f47c61561209ae710db168
Josh Moore <josh.moore@bytecodetechnologies.com>
2019-10-12 01:40:24 -0500

    fix finaly block

--- app/modules/dht_sensor/temp.py
+++ app/modules/dht_sensor/temp.py
@@ -1,10 +1,22 @@
+#!/usr/bin/python3
 import sys
 import Adafruit_DHT
 import smbus
-import time
 import logging
+import time
+import board
+import busio
+import adafruit_sht31d
+
+# Create library object using our Bus I2C port
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_sht31d.SHT31D(i2c)
+sensor.frequency = adafruit_sht31d.FREQUENCY_2
+sensor.mode = adafruit_sht31d.MODE_PERIODIC
 
 _LOGGER = logging.getLogger(__name__)
+# Get I2C bus
+_bus = smbus.SMBus(1)
 
 def read_temp():
     return read_sht_30()
@@ -16,27 +28,8 @@ def read_dht_22():
 
 def read_sht_30():
     try:
-        # Get I2C bus
-        bus = smbus.SMBus(1)
-
-        # SHT30 address, 0x44(68)
-        # Send measurement command, 0x2C(44)
-        #		0x06(06)	High repeatability measurement
-        bus.write_i2c_block_data(0x44, 0x2C, [0x06])
-
-        time.sleep(0.5)
-
-        # SHT30 address, 0x44(68)
-        # Read data back from 0x00(00), 6 bytes
-        # cTemp MSB, cTemp LSB, cTemp CRC, Humididty MSB, Humidity LSB, Humidity CRC
-        data = bus.read_i2c_block_data(0x44, 0x00, 6)
-
-        # Convert the data
-        cTemp = ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45
-        fTemp = cTemp * 1.8 + 32
-        humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+        fTemp = sensor.temperature * 1.8 + 32
+        humidity = sensor.relative_humidity
         return {'temperature': fTemp, 'humidity': humidity}
     except:
         _LOGGER.exception("SHT-30 not reporting")
-    finally:
-        return None
