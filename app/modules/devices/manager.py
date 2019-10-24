35038205b98ce2b679eac4ed3dee042e03fc5f24
Josh Moore <josh.moore@bytecodetechnologies.com>
2019-10-23 22:35:32 -0500

    remove lock for testing

--- app/modules/devices/manager.py
+++ app/modules/devices/manager.py
@@ -4,43 +4,34 @@ import json
 import os.path
 from os import path
 from .plugs import TplinkPlug
-from multiprocessing import Lock
 from app.modules.devices import (
     Device,
     OnboardRelay,
 )
 
-_LOCK = Lock()
 _DATA_DIR = '/home/weegrow/weegrow-data/devices/'
 
 def save_device(device: Device):
-    _LOCK.acquire()
     f = open(_DATA_DIR + device.encoded_alias + '.json', 'w+')
     device.save(f)
-    _LOCK.release()
     return
 
 def delete_device(encoded_alias: str):
-    _LOCK.acquire()
     full_path = _DATA_DIR + encoded_alias + '.json'
     if os.path.exists(full_path):
         os.remove(full_path)
-    _LOCK.release()
     return
 
 def retrieve_device(encoded_alias):
-    _LOCK.acquire()
     device = None
 
     file_path = _DATA_DIR + encoded_alias + '.json'
     if path.exists(file_path):
         device = _retrieve_device_from_file(file_path)
 
-    _LOCK.release()
     return device
 
 def retrieve_devices():
-    _LOCK.acquire()
     devices = []
     for file_path in os.listdir(_DATA_DIR):
         device = None
@@ -51,7 +42,6 @@ def retrieve_devices():
         if device:
             devices.append(device)
 
-    _LOCK.release()
     return devices
         
 def _retrieve_device_from_file(file_path):
