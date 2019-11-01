45ebc151360e90e8fcb6d232a43240f76bd84e7e
Josh Moore <josh.moore@bytecodetechnologies.com>
2019-11-01 09:08:07 -0500

    Revert "add toggle for individual plugs"
    This reverts commit b027202fbc1b6dd634683d9f3128086920bb1651.

--- app/modules/devices/plugs/tplinkstrip.py
+++ app/modules/devices/plugs/tplinkstrip.py
@@ -97,10 +97,7 @@ class TplinkStrip(Strip):
 
     def toggle(self, *, index: int = -1):
         """Toggle device state"""
-        if index < 0:
-            self.native_api.turn_on() if self.native_api.is_off else self.native_api.turn_on()
-        else:
-            self.native_api.turn_on(index=index) if self.native_api.get_is_off(index=index) else self.native_api.turn_off(index=index)
+        self.native_api.turn_on(index=index) if self.native_api.get_is_off(index=index) else self.native_api.turn_off(index=index)
 
     @property
     def children_info(self) -> Dict[int, Any]:
