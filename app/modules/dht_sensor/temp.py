bd8e68864381c3010357cd0a28570ffbd55c50e8
Josh Moore <josh.moore@bytecodetechnologies.com>
2019-11-04 12:03:54 -0600

    added sht-20

--- app/modules/dht_sensor/temp.py
+++ app/modules/dht_sensor/temp.py
@@ -7,24 +7,41 @@ import time
 import board
 import busio
 import adafruit_sht31d
-
-# Create library object using our Bus I2C port
-i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_sht31d.SHT31D(i2c)
-sensor.frequency = adafruit_sht31d.FREQUENCY_2
-sensor.mode = adafruit_sht31d.MODE_PERIODIC
+from sensor import SHT20
 
 _LOGGER = logging.getLogger(__name__)
-# Get I2C bus
-_bus = smbus.SMBus(1)
+
+try:
+    # Create library object using our Bus I2C port
+    i2c = busio.I2C(board.SCL, board.SDA)
+    sensor = adafruit_sht31d.SHT31D(i2c)
+    sensor.frequency = adafruit_sht31d.FREQUENCY_2
+    sensor.mode = adafruit_sht31d.MODE_PERIODIC
+except:
+    _LOGGER.exception("SHT-30 not initialized")
 
 def read_temp():
-    return read_sht_30()
+    ht_result = read_sht_30()
+    if not ht_result:
+        ht_result = read_sht_2x()
+    return ht_result
 
 def read_dht_22():
     humidity, temperature = Adafruit_DHT.read_retry(22, 4)
     temperature = 9.0/5.0 * temperature + 32
     return {'temperature': temperature, 'humidity': humidity}
+
+def read_sht_2x():
+    try:
+        # I2C bus=1, Address=0x40
+        sht = SHT20(1, 0x40)
+
+        humid = sht.humidity()
+        temp = sht.temperature() * 1.8 + 32
+
+        return {'temperature': temp, 'humidity': humid}
+    except:
+        _LOGGER.exception("SHT-2x not reporting")
 
 def read_sht_30():
     try:
