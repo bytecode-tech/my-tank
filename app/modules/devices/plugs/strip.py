38206ab704a79f2103f46ddc3e444cef794256d9
Josh Moore <josh.moore@bytecodetechnologies.com>
2020-01-06 20:07:40 -0600

    add toggle for plugs on strip

--- app/modules/devices/plugs/strip.py
+++ app/modules/devices/plugs/strip.py
@@ -34,6 +34,17 @@ class Strip(Plug):
 
         return True
 
+    def get_is_off(self, *, index: int = -1) -> Any:
+        """
+        Returns whether device is off.
+        :param index: plug index (-1 for all)
+        :return: True if device is off, False otherwise, Dict without index
+        :rtype: bool if index is provided
+                Dict[int, bool] if no index provided
+        :raises SmartStripException: index out of bounds
+        """
+        raise NotImplementedError("Device subclass needs to implement this.")
+
     def turn_on(self, *, index: int = -1):
         """
         Turns outlets on
@@ -48,5 +59,6 @@ class Strip(Plug):
         """
         raise NotImplementedError("Device subclass needs to implement this.")
 
-
-
+    def toggle(self, *, index: int = -1) -> None:
+            """Toggle device state"""
+            self.turn_on(index=index) if self.get_is_off(index=index) else self.turn_off(index=index)
