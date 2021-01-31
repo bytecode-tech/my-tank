6ad478b384a4360305bed014cef5e5a98b96ac6c
Josh Moore <josh.moore@bytecodetechnologies.com>
2021-01-30 21:22:21 -0600

    update imports

--- app/modules/devices/co2_sensor/co2_controller.py
+++ app/modules/devices/co2_sensor/co2_controller.py
@@ -1,9 +1,9 @@
 from flask import (Blueprint, request)
 from . import co2
 
-temp_controller = Blueprint('temp-controller', __name__, url_prefix='/api/co2')
+co2_controller = Blueprint('co2-controller', __name__, url_prefix='/api/co2')
 
-@temp_controller.route('/', methods=["GET"])
-def api_temp_control():
+@co2_controller.route('/', methods=["GET"])
+def api_co2_control():
 
     return co2.read()
